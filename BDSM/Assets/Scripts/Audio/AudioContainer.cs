6d2f0c55307eb7d855395e6039c86baa8a2bcbbb
Parker Levin <parkerllevin@gmail.com>
2020-01-31 02:05:07 -0800

    Sounds Effects System Functional
    HOW TO USE:
    - Add your audio clip file in Assets/Resources/Audio/SFX
    - in Assets/Audio Containers, Right Click -> Create -> Audio Container.
    - Fill in the values for the new Audio Container. Hover over the names of the fields for additional information about each one
    - Add an AudioPlayer.cs Component to the GameObject you want to play the sound from
    - Call the function AudioPlayer.PlaySFX(AudioContainer sound), passing in the new AudioContainer

--- BDSM/Assets/Scripts/Audio/AudioContainer.cs
+++ BDSM/Assets/Scripts/Audio/AudioContainer.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 [CreateAssetMenu]
 public class AudioContainer : ScriptableObject
 {
+    [Tooltip("The name of the filename of the clip in Assets/Resources/Audio/SFX\n\nExample: FootStep.wav")]
     [SerializeField] public string fileName;
 
+    [Tooltip("Proportion of the volume of the sound played")]
+    [Range(0.0f, 1.0f)]
     [SerializeField] public float volume;
 
+    [Tooltip("The minimum pitch of the sound when it is played\n\nClips will play at a random pitch between the minimum and maximum values.")]
+    [Range(-3.0f, 3.0f)]
     [SerializeField] public float pitchMin;
 
+    [Tooltip("The maximum pitch of the sound when it is played\n\nClips will play at a random pitch between the minimum and maximum values.")]
+    [Range(-3.0f, 3.0f)]
     [SerializeField] public float pitchMax;
 
+    [Tooltip("Proportion of the dimensionality of the sound played\n\nA value of 0 represents a 2D sound with no directional properties\n\nA value of 1 represents a 3D sound with full directional properties")]
+    [Range(0.0f, 1.0f)]
     [SerializeField] public float spatialBlend;
 
+    [Tooltip("The maximum distance between the AudioSource Component that plays this clip and an AudioListener Component that can hear the clip")]
     [SerializeField] public float maxDistance;
 }
--- BDSM/Assets/Scripts/Audio/AudioPlayer.cs
+++ BDSM/Assets/Scripts/Audio/AudioPlayer.cs
@@ -7,6 +7,17 @@ public class AudioPlayer : MonoBehaviour
     // Storage of all AudioClip objects ready to be played
     private Dictionary<string, AudioClip> clipStorage = new Dictionary<string, AudioClip>();
 
+    // The number of current live AudioSource objects created by this script
+    private int activeSources;
+
+    // The maximum number of AudioSource objects allowed to be live at once
+    private static int maxSources = 20;
+
+    private void Awake()
+    {
+        activeSources = 0;
+    }
+
     // takes a string filename in the path Resources/Audio/SFX
     // loads that audio file into the AudioPlayer's storage
     public void LoadClip(string name)
@@ -29,8 +40,15 @@ public class AudioPlayer : MonoBehaviour
     public void PlaySFX(AudioContainer sound)
     {
         LoadClip(sound.name);
-        if (clipStorage.ContainsKey(sound.name))
+        if (activeSources >= maxSources)
         {
+            Debug.Log("The maximum number  [" + maxSources + "] of AudioSource Components on object [" + this.gameObject + "] has been reached.");
+            return;
+        }
+        else if (clipStorage.ContainsKey(sound.name))
+        {
+            // create a new AudioSource to use
+            activeSources++;
             AudioSource tempSource = this.gameObject.AddComponent<AudioSource>();
             tempSource.volume = sound.volume;
             tempSource.pitch = Random.Range(sound.pitchMin, sound.pitchMax);
@@ -54,5 +72,6 @@ public class AudioPlayer : MonoBehaviour
         }
 
         Destroy(audioPlayer);
+        activeSources--;
     }
 }
