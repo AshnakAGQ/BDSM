faf0d08c852a03e2053fdd55017c6757b06cde36
Justin Kephart <justinpianist1@yahoo.com>
2020-02-02 00:02:21 -0800

    Functioning Gate

---
+++ BDSM/Assets/Gate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : ActivatableObject
+{
+    Animator animator;
+    bool activated = false;
+
+    public override void Activate()
+    {
+        if (!activated)
+        {
+
+            activated = true;
+            if (animator != null) animator.SetBool("activated", true);
+        }
+    }
+
+    public override void Deactivate()
+    {
+        if (activated)
+        {
+            activated = false;
+            if (animator != null) animator.SetBool("activated", false);
+        }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+}
