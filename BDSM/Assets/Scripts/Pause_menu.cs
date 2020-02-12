8712c45507b0db98983ae9fd3d2f7742041af00d
Joseph Waterbury <jwaterbu@uci.edu>
2020-02-11 17:55:09 -0800

    UI attept 1

--- BDSM/Assets/Scripts/Pause_menu.cs
+++ BDSM/Assets/Scripts/Pause_menu.cs
@@ -6,23 +6,45 @@ using UnityEngine.InputSystem;
 
 public class Pause_menu : MonoBehaviour
 {
-
+    public Pause_menu instance = null;
     public GameObject pause_menu;
     public GameObject resume_button;
     public GameObject menu_button;
     public GameObject exit_button;
     private bool paused;
 
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         checkEscapePause();
+    }
+
+    public void Pause(bool enable)
+    {
+        if (!enable)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+        pause_menu.SetActive(enable);
+        paused = true;
     }
 
     private void checkEscapePause()
